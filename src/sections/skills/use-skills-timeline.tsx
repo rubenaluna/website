4a2b58abcb39c932494b438dd5a7afd9f7ded24a
rubenaluna <rubenaluna@outlook.com>
2025-11-05 20:23:35 -0600

    Update Skills
    - Improve responsiveness
    - Update layout and styling

--- src/sections/skills/use-skills-timeline.tsx
+++ src/sections/skills/use-skills-timeline.tsx
@@ -8,47 +8,6 @@ export const useSkillsTimeline = () => {
 
     gsap.set(".skills-body", { opacity: 0 });
 
-    // Set initial state for skill cards
-    gsap.set(".skills-content > div:nth-child(1)", {
-      opacity: 0,
-      y: -50,
-      scale: 0.95,
-    });
-
-    gsap.set(".skills-be", {
-      opacity: 0,
-      x: -100,
-      scale: 0.95,
-    });
-
-    gsap.set(".skills-fe", {
-      opacity: 0,
-      y: 50,
-      scale: 0.95,
-    });
-
-    gsap.set(".skills-ops", {
-      opacity: 0,
-      x: 100,
-      scale: 0.95,
-    });
-
-    // Set initial state for skill items within cards
-    gsap.set(".skills-be > div > div", {
-      opacity: 0,
-      y: 20,
-    });
-
-    gsap.set(".skills-fe > div > div", {
-      opacity: 0,
-      y: 20,
-    });
-
-    gsap.set(".skills-ops > div > div", {
-      opacity: 0,
-      y: 20,
-    });
-
     ScrollTrigger.create({
       trigger: ".skills",
       start: "top top",
@@ -61,143 +20,13 @@ export const useSkillsTimeline = () => {
 
     ScrollTrigger.create({
       trigger: ".skills-body",
-      start: "top top",
+      start: "top top+=48px",
       end: "top top",
       scrub: true,
       toggleActions: "play none none reverse",
       animation: gsap.to(".skills-body", {
         opacity: 1,
       }),
-      onEnter: () => {
-        // Create a timeline for card animations that plays once
-        const cardsTimeline = gsap.timeline();
-
-        // Animate the title/description card
-        cardsTimeline.to(
-          ".skills-content > div:nth-child(1)",
-          {
-            opacity: 1,
-            y: 0,
-            scale: 1,
-            duration: 0.8,
-            ease: "power2.out",
-          },
-          0
-        );
-
-        // Animate the Back-End card
-        cardsTimeline.to(
-          ".skills-be",
-          {
-            opacity: 1,
-            x: 0,
-            scale: 1,
-            duration: 0.8,
-            ease: "power2.out",
-          },
-          0.2
-        );
-
-        // Animate the Front-End card
-        cardsTimeline.to(
-          ".skills-fe",
-          {
-            opacity: 1,
-            y: 0,
-            scale: 1,
-            duration: 0.8,
-            ease: "power2.out",
-          },
-          0.3
-        );
-
-        // Animate the Operations card
-        cardsTimeline.to(
-          ".skills-ops",
-          {
-            opacity: 1,
-            x: 0,
-            scale: 1,
-            duration: 0.8,
-            ease: "power2.out",
-          },
-          0.4
-        );
-
-        // Stagger animate skill items in Back-End card
-        cardsTimeline.to(
-          ".skills-be > div > div",
-          {
-            opacity: 1,
-            y: 0,
-            duration: 0.5,
-            stagger: 0.1,
-            ease: "power2.out",
-          },
-          0.5
-        );
-
-        // Stagger animate skill items in Front-End card
-        cardsTimeline.to(
-          ".skills-fe > div > div",
-          {
-            opacity: 1,
-            y: 0,
-            duration: 0.5,
-            stagger: 0.1,
-            ease: "power2.out",
-          },
-          0.6
-        );
-
-        // Stagger animate skill items in Operations card
-        cardsTimeline.to(
-          ".skills-ops > div > div",
-          {
-            opacity: 1,
-            y: 0,
-            duration: 0.5,
-            stagger: 0.1,
-            ease: "power2.out",
-          },
-          0.7
-        );
-      },
-      onLeaveBack: () => {
-        // Reset animations when scrolling back up
-        gsap.set(".skills-content > div:nth-child(1)", {
-          opacity: 0,
-          y: -50,
-          scale: 0.95,
-        });
-        gsap.set(".skills-be", {
-          opacity: 0,
-          x: -100,
-          scale: 0.95,
-        });
-        gsap.set(".skills-fe", {
-          opacity: 0,
-          y: 50,
-          scale: 0.95,
-        });
-        gsap.set(".skills-ops", {
-          opacity: 0,
-          x: 100,
-          scale: 0.95,
-        });
-        gsap.set(".skills-be > div > div", {
-          opacity: 0,
-          y: 20,
-        });
-        gsap.set(".skills-fe > div > div", {
-          opacity: 0,
-          y: 20,
-        });
-        gsap.set(".skills-ops > div > div", {
-          opacity: 0,
-          y: 20,
-        });
-      },
     });
 
     timeline.to(".skills", {
